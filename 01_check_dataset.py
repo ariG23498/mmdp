5949632e6ef6bf303978ff116669e2292846be88
ariG23498 <aritra.born2fly@gmail.com>
2025-07-04 13:20:55 +0530

    add entry point to 01

--- 01_check_dataset.py
+++ 01_check_dataset.py
@@ -5,9 +5,11 @@ from mmdp.utils import visualize_sample
 
 
 if __name__ == "__main__":
+    # Load the dataset from Hugging Face
     train_ds = load_dataset(
         path=config.train_dataset_path, name=config.train_dataset_name, split="train"
     )
 
+    # Samples one data point from the dataset at random and visualizes it
     sample = choice(train_ds)
     visualize_sample(sample)
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # MultiModal Data Pipeline
+
+## Samlpe from the Hugging Face Dataset
+`uv run 01_check_dataset.py`
+
+![](./sample.png)
+
+## Pad the Dataset
+`uv run 
